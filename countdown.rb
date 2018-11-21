1cfb4ab50451ca78acad61a06f41b6e0db7d481a
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 19:29:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,17 @@
+
+def countdown
 def countdown(x)
   while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
-  end
-  "HAPPY NEW YEAR!"
+  puts "#{x} SECOND(S)!"
+  x = x - 1
 end
- def countdown
-def countdown_with_sleep(x)
-  while x > 0
-    puts "#{x} SECOND(S)!"
-    sleep 1
-    x -= 1
-  end
-  "HAPPY NEW YEAR!"
+"HAPPY NEW YEAR!"
+end
+ def countdown_with_sleep(x)
+  while x>0
+  puts "#{x} SECOND(S)"
+  sleep 1
+  x = x - 1
+end
+"HAPPY NEW YEAR!"
 end
